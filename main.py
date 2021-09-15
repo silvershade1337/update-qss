27f085bfd629034793c20668f4ab66aa95aa843c
SilverShade <aneeshyr2@gmail.com>
2021-09-15 21:53:57 +0530

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+import re
+
+class st:
+    def __init__(self):
+        self.ssht = """\
+        font-size: 20px;
+        font-weight: Bold;
+        """
+
+    def setStyleSheet(self, stylesheet: str):
+        self.ssht = stylesheet
+
+    def styleSheet(self):
+        return self.ssht
+
+def updateStyleSheet(stylable, style: str):
+    prop, val = map(lambda strg: strg.strip(), style.split(":"))
+    val = val[:-1] if val.endswith(";") else val
+    s = stylable.styleSheet()
+    print(stylable.styleSheet(), end="\n")
+    stylable.setStyleSheet(re.sub(f"{prop} *: *[^;\n]*;?", f"{prop}: {val};", s))
+    print(stylable.styleSheet(), end="\n")
+
+updateStyleSheet(st(), "font-weight: 40px;")
